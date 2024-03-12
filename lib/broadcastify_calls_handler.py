6a031eefd624b95bb7e7b635f6205820b4513dab
thegreatcodeholio <ian@icarey.net>
2024-03-12 19:19:15 -0400

    brodcastify calls tweak

--- lib/broadcastify_calls_handler.py
+++ lib/broadcastify_calls_handler.py
@@ -65,13 +65,13 @@ def upload_to_broadcastify_calls(broadcastify_config, m4a_file_path, call_data):
                 module_logger.error("Failed to parse response from Broadcastify as JSON.")
                 return False
 
-            #base_64 = base64.b64encode(audio_file.read()).decode("utf-8")
+            base_64 = base64.b64encode(audio_file.read()).decode("utf-8")
             fields = {
                 "filefield": (os.path.basename(m4a_file_path), audio_file, 'audio/aac')
             }
 
             # Reuse the send_request function for the PUT request
-            upload_response = requests.put(upload_url,  headers={'Content-Type': 'audio/aac'}, data=audio_file.read())
+            upload_response = requests.put(upload_url,  headers={'Content-Type': 'audio/aac'}, data=base_64, files=fields)
             module_logger.debug(upload_url)
             module_logger.debug(upload_response.text)
             module_logger.debug(upload_response.json)
