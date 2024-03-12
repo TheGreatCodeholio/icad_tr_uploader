2352628986ad39fee5065c196a82a39d03371775
thegreatcodeholio <ian@icarey.net>
2024-03-12 19:29:11 -0400

    brodcastify calls tweak

--- lib/broadcastify_calls_handler.py
+++ lib/broadcastify_calls_handler.py
@@ -71,7 +71,7 @@ def upload_to_broadcastify_calls(broadcastify_config, m4a_file_path, call_data):
             }
 
             # Reuse the send_request function for the PUT request
-            upload_response = requests.put(upload_url,  headers={'Content-Type': 'audio/aac'}, data=base_64, files=fields)
+            upload_response = requests.put(upload_url,  headers={'Content-Type': 'audio/mp4'}, data=base_64, files=fields)
             module_logger.debug(upload_url)
             module_logger.debug(upload_response.text)
             module_logger.debug(upload_response.json)
